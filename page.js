0db2536de22d645dfbda77f51dca044b14a315bb
Claudia Renata <maharanic20@gmail.com>
2019-11-29 01:08:33 +0700

    front end done

---
+++ page.js
@@ -0,0 +1,14 @@
+function hidetabelPlaylist() {
+    document.getElementById('tabelPlaylist').style.display='none';
+    
+}
+
+function showtabelhasilSearch(){
+    document.getElementById('tabel-hasil-search').style.visibility='visible'; 
+}
+
+function showHideTabel(){
+    hidetabelPlaylist();
+    showtabelhasilSearch(); 
+}
+
