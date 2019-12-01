db3910024d27fda49064ecdb6eab79236bec0e75
Claudia Renata <maharanic20@gmail.com>
2019-12-01 16:43:35 +0700

    AA

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['CORS_HEADERS'] = 'content-type'
 app.config['CORS_RESOURCES'] = '*'
 app.config['CORS_METHODS'] = "GET,POST,OPTIONS"
 
