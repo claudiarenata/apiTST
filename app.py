3998fbc94d3e31c6a1398641d1d24eb8691da5d4
Claudia Renata <maharanic20@gmail.com>
2019-12-01 15:59:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -44,10 +44,10 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 @app.after_request
 def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-#   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  return response
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
 
 # get data from spotify #
 @app.route('/api/playlist', methods=['POST', 'OPTIONS'])
