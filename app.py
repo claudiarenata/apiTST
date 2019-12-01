a28406c3363943d2f68a0dce97b976ded17bd6bb
Claudia Renata <maharanic20@gmail.com>
2019-12-01 16:38:22 +0700

    push addPlaylist

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+#app.config['CORS_HEADERS'] = 'Content-Type'
 # app.config['CORS_RESOURCES'] = {r"/apis/*":{"origins":"*"}}
-app.config['CORS_METHODS'] = "GET,POST,OPTIONS"
+#app.config['CORS_METHODS'] = "GET,POST,OPTIONS"
 
 
 mysql = MySQL()
@@ -51,26 +51,6 @@ def after_request(response):
 
 # get data from spotify #
 @app.route('/api/playlist', methods=['POST', 'OPTIONS'])
-@cross_origin()
-# def api_create_order():
-#     if request.method == "OPTIONS": # CORS preflight
-#         return _build_cors_prelight_response()
-#     elif request.method == "POST": # The actual request following the preflight
-#         order = OrderModel.create(...) # Whatever.
-#         return _corsify_actual_response(jsonify(order.to_dict()))
-#     else : 
-#         raise RuntimeError("Wierd - don't know how to handle method {}".format(request.method))
-
-# def _build_cors_prelight_response():
-#     response = make_response()
-#     response.headers.add("Access-Control-Allow-Origin", "*")
-#     response.headers.add('Access-Control-Allow-Headers', "*")
-#     response.headers.add('Access-Control-Allow-Methods', "*")
-#     return response
-
-# def _corsify_actual_response(response):
-#     response.headers.add("Access-Control-Allow-Origin", "*")
-#     return response
 
 def songs():
     global conn, cursor
